261748ac2d10e8676365a83b20ba7a4220d79090
Vladislav Panfilov <andromalak222@gmail.com>
2024-06-25 13:43:27 +0500

    oil config update

--- nvim/lua/hbom/lazy/plugins/oil.lua
+++ nvim/lua/hbom/lazy/plugins/oil.lua
@@ -3,12 +3,24 @@ return {
 	dependencies = {
 		'nvim-tree/nvim-web-devicons',
 	},
-	config = function ()
-		require('oil').setup {
-			keymaps = {
-				['<C-h>'] = false,
-				['<C-l>'] = false,
-			},
+	opts = {
+		keymaps = {
+			['<C-h>'] = false,
+			['<C-l>'] = false,
+		},
+		view_options = {
+			show_hidden = true,
+			is_always_hidden = function(name, bufnr)
+				local should_be_hidden = { }
+				should_be_hidden['.git'] = true
+				should_be_hidden['..'] = true
+
+				if (should_be_hidden[name] ~= nil) then
+					return true
+				end
+
+				return false
+			end,
 		}
-	end
+	},
 }
