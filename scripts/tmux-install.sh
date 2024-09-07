043992e610cc7c2f4aa144d129c74379737426d9
Vladislav Panfilov <andromalak222@gmail.com>
2024-09-07 15:30:34 +0500

    update

--- scripts/tmux-install.sh
+++ scripts/tmux-install.sh
@@ -1,15 +1,12 @@
 #! /bin/bash
 
 # install tmux
-pacman -S tmux
-echo "tmux installed"
+sudo pacman -S tmux
 
 # link config file
 rm ~/.tmux.conf
 ln -s "$(pwd)/tmux/.tmux.conf" ~/.tmux.conf
-echo "tmux config linked"
 
 # link custom folder
 rm ~/.tmux/plugins/tmux/custom
 ln -s "$(pwd)/tmux/custom" ~/.tmux/plugins/tmux/custom
-echo "custom plugins linked"
