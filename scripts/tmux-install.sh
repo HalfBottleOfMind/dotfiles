b717cc21ec8eb89f17b52857cde9663e6689e278
Vladislav Panfilov <andromalak222@gmail.com>
2024-09-07 15:59:49 +0500

    tmux custom removed

--- scripts/tmux-install.sh
+++ scripts/tmux-install.sh
@@ -1,10 +1,8 @@
 #! /bin/bash
 
-mkdir -p ~/.tmux/plugins/tmux
-ln -s "$(pwd)/tmux/custom" ~/.tmux/plugins/tmux/custom
 ln -s "$(pwd)/tmux/.tmux.conf" ~/.tmux.conf
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # install tmux
-sudo pacman -S tmux
+sudo pacman -S --noconfirm tmux
