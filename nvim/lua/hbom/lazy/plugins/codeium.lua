b463839f49f9cc74106cfae5156ad7ceb7e5802b
Vladislav Panfilov <andromalak222@gmail.com>
2024-09-17 03:56:39 +0500

    codeium default chat port

--- nvim/lua/hbom/lazy/plugins/codeium.lua
+++ nvim/lua/hbom/lazy/plugins/codeium.lua
@@ -1,4 +1,10 @@
 return {
 	'Exafunction/codeium.vim',
-	event = 'BufEnter'
+	event = 'BufEnter',
+	config = function()
+		vim.g.codeium_port_config = {
+			web_server = 8888,
+			chat_client = 9999,
+		}
+	end,
 }
