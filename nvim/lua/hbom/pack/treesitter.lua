d96efdab85c034f832167a1d26594521b258db6f
Vladislav Panfilov <andromalak222@gmail.com>
2026-05-13 13:29:23 +0500

    fix indent

--- nvim/lua/hbom/pack/treesitter.lua
+++ nvim/lua/hbom/pack/treesitter.lua
@@ -1,11 +1,12 @@
 vim.pack.add({'https://github.com/nvim-treesitter/nvim-treesitter'})
 
 vim.api.nvim_create_autocmd('FileType', {
-  pattern = {
-     '*',
-  },
-  callback = function()
-    vim.treesitter.start()
-    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-  end
+	pattern = {
+		'lua',
+		'php',
+	},
+	callback = function()
+		vim.treesitter.start()
+		vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+	end
 })
